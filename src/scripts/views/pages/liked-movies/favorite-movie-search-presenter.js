8dc89d2f0d7f68b347c5dc4c82f8f64e84c8ba3e
Samuel Yusa <Samuelyusa@gmail.com>
2023-06-19 20:47:49 +0700

    Commit

--- src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
+++ src/scripts/views/pages/liked-movies/favorite-movie-search-presenter.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable class-methods-use-this */
 class FavoriteMovieSearchPresenter {
-  constructor({ favoriteMovies }) {
-    this._listenToSearchRequestByUser();
+  constructor({ favoriteMovies, view }) {
+    this._view = view;
     this._favoriteMovies = favoriteMovies;
+    this._listenToSearchRequestByUser();
   }
 
   _listenToSearchRequestByUser() {
-    this._queryElement = document.getElementById('query');
-    this._queryElement.addEventListener('change', (event) => {
-      this._searchMovies(event.target.value);
+    this._view.runWhenUserIsSearching((latestQuery) => {
+      this._searchMovies(latestQuery);
     });
   }
 
@@ -27,22 +27,7 @@ class FavoriteMovieSearchPresenter {
   }
 
   _showFoundMovies(movies) {
-    // console.log(movies);
-    let html;
-
-    if (movies?.length > 0) {
-      html = movies.reduce(
-        (carry, movie) => carry.concat(`<li class="movie"><span class="movie__title">${movie.title || '-'}</span></li>`),
-        '',
-      );
-    } else {
-      html = '<div class="movies__not__found">Film tidak ditemukan</div>';
-    }
-
-    document.querySelector('.movies').innerHTML = html;
-
-    document.getElementById('movie-search-container')
-      .dispatchEvent(new Event('movies:searched:updated'));
+    this._view.showMovies(movies);
   }
 
   get latestQuery() {
